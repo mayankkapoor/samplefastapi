bd6260fd1aae6749fccc8aa369e2c3a4f3b43116
Amit Soren <amit@setupenzyme.com>
2021-09-02 06:37:50 +0000

    Updated database.py

--- blog/database.py
+++ blog/database.py
@@ -4,14 +4,16 @@ from sqlalchemy.orm import sessionmaker
 from decouple import config
 
 # SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
-user = config("POSTGRES_USER")
-pwd = config("POSTGRES_PASSWORD")
-db = config("POSTGRES_DB")
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{pwd}@postgresserver/{db}"
+user = config("POSTGRES_USER", cast=str)
+pwd = config("POSTGRES_PASSWORD", cast=str)
+db = config("POSTGRES_DB", cast=str)
+host = config("POSTGRES_SERVER", cast=str)
+port = config("POSTGRES_PORT", cast=int)
+SQLALCHAMY_DATABASE_URL = f"postgresql://{user}:{pwd}@{host}:{port}/{db}"
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
-
+# engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
+#                        "check_same_thread": False})
+engine = create_engine(SQLALCHAMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
 Base = declarative_base()
